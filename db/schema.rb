badf119368439d4e204ed46c3f180e680896d82d
Pixelephant Media <pixelephant@pixelephant.hu>
2012-05-24 11:04:36 +0200

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -109,12 +109,6 @@ ActiveRecord::Schema.define(:version => 20120522113945) do
     t.datetime "updated_at",      :null => false
   end
 
-  create_table "in_out_prices", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "inprices", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
