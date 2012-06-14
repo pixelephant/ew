144269bed9bbedaf11ab0e6c781eea918cf5c08f
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-06-14 14:34:21 +0200

    admin label method
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,10 +9,18 @@ RailsAdmin.config do |config|
 	config.model TravelOffer do
 		include_all_fields
 		exclude_fields :md5
+
+		object_label_method do
+			:travel_offer_label_method
+		end
 	end
 
 	def travel_time_label_method
 		self.travel_offer.travel_name
+	end
+
+	def travel_offer_label_method
+		self.travel_name
 	end
 
 end
