652e2f466fc0c0aa4773764b37241c45df30e31e
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-09-11 11:49:43 +0200

    győri eto
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/controllers/exclusive_controller.rb
+++ app/controllers/exclusive_controller.rb
@@ -2,7 +2,7 @@
 class ExclusiveController < ApplicationController
 	layout "application"
 
-	caches_page :hungaroring, :fcbarcelona
+	caches_page :hungaroring, :fcbarcelona, :gyorietovip
 
 	def hungaroring
 		@title = "Exkluzív hírdetési ajánlat a Hungaroringre - "
@@ -14,6 +14,12 @@ class ExclusiveController < ApplicationController
 		@title = "Eddzen együtt az FC Barcelona edzőivel - "
 		@description = "5 napon át, 90 perces edzés (reggel) az FC Barcelona profi edzőivel."
 		render "barcelona"
+	end
+
+	def gyorietovip
+		@title = "Győri ETO FC stadion VIP páholyok értékesítése! - "
+		@description = "Győri ETO FC stadion VIP páholyok értékesítése!"
+		render "gyorietovip"
 	end
 
 end
